dd605a58b797f7a0ed552ef941943244dd1e2e1f
A.rakhmatullin <A.rakhmatullin@x5.ru>
2021-12-09 10:17:00 +0700

    Test new version

--- tools/ci/core/build.sh
+++ tools/ci/core/build.sh
@@ -19,13 +19,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Resolve commit range for current build
 LAST_SUCCESSFUL_COMMIT=$(${CI_PLUGIN} hash last)
 #LAST_SUCCESSFUL_COMMIT="d88f95dfe5835daf043d36c21d01c0d754e41538"
-#echo "Last commit: ${LAST_SUCCESSFUL_COMMIT}"
-#if [[ ${LAST_SUCCESSFUL_COMMIT} == "null" ]]; then
-#    COMMIT_RANGE="origin/master"
-#else
-#    COMMIT_RANGE="$(${CI_PLUGIN} hash current)..${LAST_SUCCESSFUL_COMMIT}"
-#fi
-#echo "Commit range: $COMMIT_RANGE"
+echo "Last commit: ${LAST_SUCCESSFUL_COMMIT}"
+if [[ ${LAST_SUCCESSFUL_COMMIT} == "null" ]]; then
+    COMMIT_RANGE=$GITHUB_REF
+else
+    COMMIT_RANGE="$(${CI_PLUGIN} hash current)..${LAST_SUCCESSFUL_COMMIT}"
+fi
+echo "Commit range: $COMMIT_RANGE"
 
 # Ensure we have all changes from last successful build
 if [[ -f $(git rev-parse --git-dir)/shallow ]]; then
@@ -36,7 +36,7 @@ if [[ -f $(git rev-parse --git-dir)/shallow ]]; then
         until git show ${LAST_SUCCESSFUL_COMMIT} > /dev/null 2>&1
         do
             DEPTH=$((DEPTH+5))
-#            echo "Last commit not fetched yet. Fetching depth $DEPTH."
+            echo "Last commit not fetched yet. Fetching depth $DEPTH."
             git fetch --depth=$DEPTH
         done
     fi
@@ -46,11 +46,11 @@ fi
 #PROJECTS_TO_BUILD=$($DIR/list-projects-to-build.sh $COMMIT_RANGE)
 PROJECTS_TO_BUILD=$(./pants --changed-since=$LAST_SUCCESSFUL_COMMIT --changed-dependees=transitive list | xargs ./pants filter --target-type=pex_binary)
 
-# If nothing to build inform and exit
-#if [[ -z "$PROJECTS_TO_BUILD" ]]; then
-#    echo "No projects to build"
-#    exit 0
-#fi
+ If nothing to build inform and exit
+if [[ -z "$PROJECTS_TO_BUILD" ]]; then
+    echo "No projects to build"
+    exit 0
+fi
 
 
 #echo "Following projects need to be built"
@@ -72,5 +72,5 @@ do
 done <<< "$PROJECTS_TO_BUILD"
 #echo "${PARSED_PROJECTS[@]}"
 #printf '%s' "${PARSED_PROJECTS[@]}" | jq -R . | jq -s -c 'add'
-echo "${PARSED_PROJECTS[@]}" | tr ' ' ','
+export OUTPUT=$(echo "${PARSED_PROJECTS[@]}" | tr ' ' ',')
 #jq --compact-output --null-input '$PARSED_PROJECTS' --args "${PARSED_PROJECTS[@]}"
